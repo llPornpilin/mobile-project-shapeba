91fb31b276bb8d35f98f9b77bddd8943edfd82ad
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2023-09-08 15:41:30 +0700

    components

--- ShapeBaProject/src/screens/Dashboard/DashboardDayScreen.js
+++ ShapeBaProject/src/screens/Dashboard/DashboardDayScreen.js
@@ -1,22 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DonutChartContainer from '../../components/donutChart';
 
 
 const DashboardDayScreen = () => {
     return (
         <View style={styles.container}>
             <Text>Dashboard Day</Text>
+            <DonutChartContainer />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
 
-  export default DashboardDayScreen;
+export default DashboardDayScreen;
