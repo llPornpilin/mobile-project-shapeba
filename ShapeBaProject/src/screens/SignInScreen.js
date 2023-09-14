044bf50ec7b4d40599fea6f37b73ca848563990c
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2023-09-14 21:16:06 +0700

    resolve merge

--- ShapeBaProject/src/screens/SignInScreen.js
+++ ShapeBaProject/src/screens/SignInScreen.js
@@ -10,22 +10,31 @@ import {
 } from "react-native";
 import React from "react";
 
-const SignUp = () => {
+// Page
+// import SignUpScreen from './SignUpScreen';
+
+
+const SignInScreen = ({ navigation, props }) => {
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor:'#fff', alignItems:'center' }}>
-      <Image
-        source={require("../../assets/img/Icon.jpg")}
-        style={{ height: 150, width: 150 }}
-      />
-      <Text style={styles.LabelFullName}>FullName</Text>
-      <TextInput
-        style={styles.inputFullName}
-        value="Lingling123"
-        placeholder="Enter Fullname"
-      />
-      <Text style={styles.LabelUsername}>User Name</Text>
+    <View style={styles.container}>
+      <View style={styles.img}>
+        <Image
+          source={require("../../assets/img/Icon.jpg")}
+          style={{ width: '100%', height: '100%', resizeMode: 'contain', alignSelf: 'center' }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>FullName</Text>
         <TextInput
-          style={styles.inputUsername}
+          style={styles.input}
+          value="Lingling123"
+          placeholder="Enter Fullname"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>User Name</Text>
+        <TextInput
+          style={styles.input}
           value="Lingling123"
           placeholder="Enter UserName"
         />
@@ -36,140 +45,109 @@ const SignUp = () => {
           value="Lingling123"
           placeholder="Enter Password"
         />
-         <Text style={styles.LabelConfirm}>Comfirm Password</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Comfirm Password</Text>
         <TextInput
-          style={styles.inputConfirm}
+          style={styles.input}
           secureTextEntry
           value="Lingling123"
           placeholder="Confirm Password"
         />
-        <TouchableOpacity
-        style={styles.btn1}
-        onPress={() => props.navigations.navigate("#")}
-      >
-        <Text style={styles.btnText1}>Sign In</Text>
+      </View>
+      <TouchableOpacity style={styles.btn1} onPress={() => props.navigations.navigate("#")}>
+        <Text style={styles.btnText1}>Sign up</Text>
       </TouchableOpacity>
-      <Text style={styles.orText}>or</Text>
-      <TouchableOpacity style={styles.btn2} onPress={()=> props.navigations.navigate("#")}>
-          <Text style={styles.btnText2}>Sign in with google</Text>
+      <View style={styles.orContainer}>
+        <Text style={styles.orText}>or</Text>
+      </View>
+      <TouchableOpacity style={styles.btn2} onPress={() => props.navigations.navigate("#")}>
+        <Text style={styles.btnText2}>Sign up with google</Text>
+      </TouchableOpacity>
+      <View style={styles.signupContainer}>
+        <Text>Don’t have an account?</Text>
+        <TouchableOpacity style={styles.btn3} onPress={() => props.navigations.navigate("#")}>
+          <Text style={{ color: "#EC744A", fontWeight: 'bold' }}>Sign Up</Text>
         </TouchableOpacity>
-        <View style={styles.signupContainer}>
-          <Text>Don’t have an account?</Text>
-          <TouchableOpacity style={styles.btn3} onPress={()=> props.navigations.navigate("#")}>
-            <Text style={{color: "#EC744A", fontWeight: 'bold', marginLeft:6}}>Sign Up</Text>
-          </TouchableOpacity>
-        </View>
-    </ScrollView>
-  );
-};
+      </View>
+    </View>
+  )
+}
 
-export default SignUp;
 
 const styles = StyleSheet.create({
-  LabelFullName: {
-    color: "#575757",
-    fontWeight: "bold",
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img: {
+    width: 120,
+    height: 120,
+    overflow: 'hidden',
+    marginBottom: 0.2,
+    marginTop: 10
+  },
+  inputContainer: {
+    marginBottom: 10,
+    width: '80%',
+  },
+  inputLabel: {
+    color: '#575757',
+    fontWeight: 'bold',
     fontSize: 17,
-    marginRight:260,
-    marginBottom:5,
   },
-  inputFullName: {
-    marginTop: 10,
-    backgroundColor: "#f0f0f0",
-    color: "#333",
-    borderRadius: 30,
-    height: 45,
-    width: 340,
-    padding: 15,
-    marginBottom:15
-  },
-  LabelUsername: {
-    color: "#575757",
-    fontWeight: "bold",
-    fontSize: 17,
-    marginRight:250,
-    marginBottom:5,
-  },
-  inputUsername: {
-    marginTop: 10,
-    backgroundColor: "#f0f0f0",
-    color: "#333",
-    borderRadius: 30,
-    height: 45,
-    width: 340,
-    padding: 15,
-    marginBottom:15
-  },
-  LabelPassword: {
-    color: "#575757",
-    fontWeight: "bold",
-    fontSize: 17,
-    marginRight:255,
-    marginBottom:5,
-  },
-  inputPassword: {
-    marginTop: 10,
-    backgroundColor: "#f0f0f0",
-    color: "#333",
-    borderRadius: 30,
-    height: 45,
-    width: 340,
-    padding: 15,
-    marginBottom:15
-  },
-  LabelConfirm: {
-    color: "#575757",
-    fontWeight: "bold",
-    fontSize: 17,
-    marginRight:180,
-    marginBottom:5,
-  },
-  inputConfirm: {
-    marginTop: 10,
-    backgroundColor: "#f0f0f0",
-    color: "#333",
-    borderRadius: 30,
-    height: 45,
-    width: 340,
-    padding: 15,
-    marginBottom:15
+  input: {
+    marginTop: 2,
+    backgroundColor: '#f0f0f0',
+    color: '#333',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: '100%',
   },
   btn1: {
-    height: 50,
-    width: 340,
+    marginTop: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 130,
     borderRadius: 30,
-    backgroundColor: "#EC744A",
-    marginBottom:10,
-    elevation: 5,
-    marginTop:10
+    backgroundColor: '#EC744A',
+    marginBottom: 2,
+    flexDirection: 'column'
   },
   btn2: {
     marginTop: 10,
-    height: 50,
-    width: 340,
+    paddingVertical: 16,
+    paddingHorizontal: 100,
     borderRadius: 30,
-    borderColor:'#025146',
+    borderColor: '#025146',
     backgroundColor: '#FFFFFF',
+    marginBottom: 1,
+    flexDirection: 'column',
     elevation: 5
   },
   btn3: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     borderRadius: 30,
     backgroundColor: '#fff',
+    alignItems: 'center'
   },
   btnText1: {
-    color: "#FFFFFF",
-    fontWeight: "bold",
-    fontSize: 17,
-    textAlign: "center",
-    marginTop: 10,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20
   },
   btnText2: {
-      color: '#EC744A',
-      fontWeight: "bold",
-    fontSize: 17,
-    textAlign: "center",
-    marginTop: 10,
-    },
+    color: '#EC744A',
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  orContainer: {
+    marginTop: 0.2,
+    marginBottom: 0.2
+  },
   orText: {
     color: '#575757',
     fontWeight: 'normal',
@@ -181,3 +159,5 @@ const styles = StyleSheet.create({
     marginTop: 20,
   }
 });
+
+export default SignInScreen
